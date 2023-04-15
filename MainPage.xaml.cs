56d4408406afbd7c8a90268af6a0af493931f9d2
gggkamil <74270259+gggkamil@users.noreply.github.com>
2023-04-15 23:39:28 +0200

    NNumerClearing

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,12 +14,15 @@ public partial class MainPage : ContentPage
 	void ClickedOnClear(object sender, EventArgs e)
 	{
 		currentState = 0;
+        this.resultText.Text = "0";
 
-	}
+    }
 	void ClickedOnNumber(object sender, EventArgs e)
 	{
 		Button button= (Button)sender;
 		string pressed = button.Text;
-	}
+
+        this.resultText.Text += pressed;
+    }
 }
 
