9d1e1aa11ad60800b70f13c2d9f21b53c8ca5250
gggkamil <74270259+gggkamil@users.noreply.github.com>
2023-04-15 23:19:58 +0200

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static double Calculate(double x, double y, string mathOperator)
+        {
+            double result = 0;
+
+            switch(mathOperator)
+            {
+                case "+":
+                    result = x+y;
+                    break;
+                case "-":
+                    result = x-y;
+                    break;
+                case "*":
+                    result = x*y; 
+                    break;
+                case "/":
+                    result = x/y;
+                    break;
+                case "%":
+                    result = x*y/y;
+                    break;
+
+            }
+            return result;
+        }
+    }
+
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+		ClickedOnClear(this, null);
+	}
+
+	void ClickedOnClear(object sender, EventArgs e)
+	{
+
+	}
+}
+
